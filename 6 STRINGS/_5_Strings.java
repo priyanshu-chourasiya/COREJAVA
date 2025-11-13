536c1b5b4c25a62283bc0528d57509fe3057b2d9
Priyanshu Chourasiya <priyxsh005@gmail.com>
2025-11-13 19:48:16 +0530

    DONE

--- 6 STRINGS/_5_Strings.java
+++ 6 STRINGS/_5_Strings.java
@@ -1,5 +1,15 @@
 public class _5_Strings {
     public static void main(String[] args) {
-        
+        String s1= "Raj aRam Mohan Roy";
+        String[] str = s1.split(" ");
+        System.out.println(str.length);
+        for(String i : str){
+            System.out.print(i);
+        }
+        System.out.println();
+
+        String s2 = "   Raja Ram Mohan Roy  ";
+        System.out.println(s2);
+        System.out.println(s2.trim());
     }
 }
