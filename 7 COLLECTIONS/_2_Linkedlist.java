94319da878cdd6131c028d13656592e9cc60bf4d
Priyanshu Chourasiya <priyxsh005@gmail.com>
2025-11-14 10:48:03 +0530

    Commit

--- 7 COLLECTIONS/_2_Linkedlist.java
+++ 7 COLLECTIONS/_2_Linkedlist.java
@@ -1,7 +1,4 @@
-
 import java.util.LinkedList;
-
-
 public class _2_Linkedlist {
     public static void main(String[] args) {
         LinkedList ll1 = new LinkedList<>();
@@ -12,8 +9,18 @@ public class _2_Linkedlist {
         ll1.addLast(78);
         ll1.add(2,11);
         System.out.println(ll1);
-        ll1.clear();
-        System.out.println(ll1);
+        //ll1.clear();
+        //System.out.println(ll1);
         System.out.println(ll1.contains(45));
+        ll1.offer("DevOps");
+        System.out.println(ll1);
+        ll1.offer("HELLO");
+        System.out.println(ll1);
+        ll1.offerFirst("Sprint");
+        System.out.println(ll1);
+        System.out.println(ll1.peek());
+        System.out.println(ll1.peekLast());
+        System.out.println(ll1.poll()); // removes first element
+        System.out.println(ll1);
     }
 }
---
+++ 7 COLLECTIONS/_3_Arraydequeue.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayDeque;
+
+public class _3_Arraydequeue {
+    public static void main(String[] args) {
+        ArrayDeque ad1 = new ArrayDeque<>();
+        ad1.add(10);
+        ad1.add(12);
+        ad1.add(15);
+        ad1.add(48);
+        ad1.add("ANC");
+        System.out.println(ad1);
+    }
+}
