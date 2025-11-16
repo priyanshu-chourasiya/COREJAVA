4efe45ad73e4744025b65dda7765eff2466e7260
Priyanshu <priyxsh005@gmail.com>
2025-11-16 11:49:20 +0530

    Add basic multithreading example
    Introduces _1_Multithreading.java, demonstrating a simple program that prints messages before and after a 5-second pause using Thread.sleep.

---
+++ 11 MULTITHREADING/_1_Multithreading.java
@@ -0,0 +1,8 @@
+
+public class _1_Multithreading {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Application started");
+        Thread.sleep(5000);
+        System.out.println("Application terminated");
+    }
+}
